2855b0fd324bd18c5baaba664df0a8ae7a236bbc
C.J.Kent <cjkent@hotmail.com>
2019-11-12 22:38:40 +0000

    Inherit annotation

--- src/main/java/org/joda/beans/ser/DeserializerProvider.java
+++ src/main/java/org/joda/beans/ser/DeserializerProvider.java
@@ -6,12 +6,14 @@
 package org.joda.beans.ser;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface DeserializerProvider {
 
   Class<? extends SerDeserializerProvider> value();
